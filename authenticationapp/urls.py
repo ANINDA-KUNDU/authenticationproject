c72e6ad0bbeb967eed5c1e2aaf639fd8a13aa1fe
ANINDA-KUNDU <kunduaninda@yahoo.com>
2025-02-15 11:17:06 +0530

    commit

---
+++ authenticationapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Home, name = "home"),
+    path('register', views.Register, name = "register"),
+    path('login', views.Login, name = "login"),
+    path('logout', views.Logout, name = "logout"),
+    path('forgot-password', views.ForgotPassword, name = "forgot-password"),
+    path('password-reset-sent/<str:reset_id>', views.PasswordResetSent, name = "password-reset-sent"),
+    path('reset-password/<str:reset_id>', views.PasswordReset, name = "reset-password"),
+]
